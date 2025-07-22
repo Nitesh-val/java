1ea7a08601fccf5a43aadd27fa0372eaf7eca954
Nitesh Thammu <yeetmaster068@gmail.com>
2025-07-22 14:48:40 +0530

    Add files via upload

---
+++ WideningTypeCasting.java
@@ -0,0 +1,15 @@
+package oop;
+
+public class WideningTypeCasting {
+	public static void main(String[] args) 
+	{
+	 int i = 10;
+	
+	long l = i;
+	double d = i;
+	System.out.println("Integer: " + i); 
+	System.out.println("Long: " + l); 
+	System.out.println("Double: " + d); 
+	}
+
+}
